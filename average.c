6342c8f8e605bb75555599997c43426e5adef2bc
lxs <1229589839@qq.com>
2022-04-21 12:24:04 +0800

    add average.c

--- average.c
+++ average.c
@@ -3,9 +3,9 @@ int main()
 {
      int a,b;
      printf("please input a :\r\n");
-     scanf("%d",a);
+     scanf("%d",&a);
      printf("please input b :\r\n");
-     scanf("%d",b);
+     scanf("%d",&b);
      double c = (a + b) / 2;
      printf("the average of %d and %d is %.2f\r\n",a,b,c);
   
